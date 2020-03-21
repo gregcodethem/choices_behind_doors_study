5908c28084fa1c7fefcc46fc44752cdc59656141
Greg Shepley <gregshepley@yahoo.co.uk>
2020-03-21 21:27:38 +0000

    memory game models improved and added

--- doorgame/models.py
+++ doorgame/models.py
@@ -3,36 +3,51 @@ from django.contrib.auth.models import User
 
 
 class Trial(models.Model):
-	user = models.ForeignKey(
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         default=None,
         null=True
     )
 
+
 class MemoryGame(models.Model):
-	user = models.ForeignKey(
-		User,
+    trial = models.ForeignKey(
+        Trial,
         on_delete=models.CASCADE,
         default=None,
         null=True
-		)
+    )
+    box_1 = models.BooleanField(default=False)
+    box_2 = models.BooleanField(default=False)
+    box_3 = models.BooleanField(default=False)
+    box_4 = models.BooleanField(default=False)
+    box_5 = models.BooleanField(default=False)
+    box_6 = models.BooleanField(default=False)
+    box_7 = models.BooleanField(default=False)
+    box_8 = models.BooleanField(default=False)
+    box_9 = models.BooleanField(default=False)
+
+    initial_or_final = models.CharField(max_length=15, default="")
+
+
 
 class Choice(models.Model):
     door_number = models.IntegerField(default=0)
     first_or_second_choice = models.IntegerField(default=0)
     trial = models.ForeignKey(
-    	Trial,
-    	on_delete=models.CASCADE,
+        Trial,
+        on_delete=models.CASCADE,
         default=None,
         null=True
     )
 
+
 class Result(models.Model):
-	door_number = models.IntegerField(default=0)
-	trial = models.ForeignKey(
-    	Trial,
-    	on_delete=models.CASCADE,
+    door_number = models.IntegerField(default=0)
+    trial = models.ForeignKey(
+        Trial,
+        on_delete=models.CASCADE,
         default=None,
         null=True
     )
