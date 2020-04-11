c8368876d5a1fe966130d9ba12ccaef46bcb9991
Greg Shepley <gregshepley@yahoo.co.uk>
2020-04-11 11:56:36 +0100

    added passenger file

---
+++ passenger_wsgi.py
@@ -0,0 +1,20 @@
+import sys
+import os
+cwd = os.getcwd()
+INTERP = cwd+"/env/bin/python"
+# INTERP is present twice so that the new python interpreter
+# knows the actual executable path
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/doorgame')  # You must add your project here
+
+sys.path.insert(0, cwd + '/env/bin')
+sys.path.insert(
+    0, cwd + '/env/lib/python3.7/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
