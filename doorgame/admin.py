213dc0977655a7899ecf39e87f630b7db81a42e4
Greg Shepley <gregshepley@yahoo.co.uk>
2020-04-11 08:47:10 +0100

    added profile model to store easy or hard dots and updated dots

--- doorgame/admin.py
+++ doorgame/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Trial, Choice, MemoryGame, SurveyAnswers, MemoryGameList
+from .models import Trial, Choice, MemoryGame, SurveyAnswers, MemoryGameList, Profile
 
 
 admin.site.register(Trial)
@@ -8,3 +8,4 @@ admin.site.register(Choice)
 admin.site.register(MemoryGame)
 admin.site.register(MemoryGameList)
 admin.site.register(SurveyAnswers)
+admin.site.register(Profile)
