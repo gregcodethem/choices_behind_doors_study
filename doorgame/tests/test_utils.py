ed7dc81ec3890e55ca0b43637c9e0989f4e8b2ee
Greg Shepley <gregshepley@yahoo.co.uk>
2020-04-11 09:01:42 +0100

    dot list now expanded to 61 patterns if less than 61

--- doorgame/tests/test_utils.py
+++ doorgame/tests/test_utils.py
@@ -1,11 +1,13 @@
 from doorgame.utils import memory_game_bool_matrix
 
 from doorgame.all_dots import all_dots_list
+from doorgame.easy_dots import easy_dots_list
 
 from .test_views_base import BaseTest
 
 
 class MemoryGameTest(BaseTest):
 
-	def test_memory_game_bool_matrix(self):
-		pass
+	def test_memory_game_bool_matrix_small_list(self):
+		expanded_matrix = memory_game_bool_matrix(easy_dots_list)
+		self.assertEqual(len(expanded_matrix), 61)
--- doorgame/utils.py
+++ doorgame/utils.py
@@ -4,22 +4,31 @@ from .all_dots import all_dots_list
 from config.settings import TRIAL_LIMIT
 from .models import MemoryGame
 
+
 def memory_game_bool_matrix(dot_list):
-	shuffle(dot_list)
-	bool_list_matrix = []
-	for pattern in dot_list:
-		pattern_bool = []
-		for square in pattern:
-			if square == '0':
-				pattern_bool.append(False)
-			if square == '1':
-				pattern_bool.append(True)
-		bool_list_matrix.append(pattern_bool)
-	return bool_list_matrix
+        # if dots list shorter than trial limit
+    # then repeat dots list until it gets to 61
+    while len(dot_list) < 61:
+        i = 0
+        dot_list.append(dot_list[i])
+        i = i + 1
+
+    shuffle(dot_list)
+    bool_list_matrix = []
+    for pattern in dot_list:
+        pattern_bool = []
+        for square in pattern:
+            if square == '0':
+                pattern_bool.append(False)
+            if square == '1':
+                pattern_bool.append(True)
+        bool_list_matrix.append(pattern_bool)
+    return bool_list_matrix
 
 
 def add_memory_games(MemoryGameList):
     bool_matrix = memory_game_bool_matrix(all_dots_list)
+
     for i in range(0, TRIAL_LIMIT):
         _MemoryGame = MemoryGame()
         bool_list = bool_matrix[i]
