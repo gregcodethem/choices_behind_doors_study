4eceff169c692ad129c4c2d0c25ea5f4ebd94b6d
Greg Shepley <gregshepley@yahoo.co.uk>
2020-04-12 16:57:15 +0100

    added so that it sends to thankyou and also can log out at end

--- doorgame/views.py
+++ doorgame/views.py
@@ -321,6 +321,9 @@ def home_page_memory_game(request, username):
 
     # logout_if_reached_the_limit(request)
     user_logged_in = request.user
+    if user_logged_in.profile.trial_completed >= TRIAL_LIMIT:
+        return final_completion()
+        
     trials_for_this_user = Trial.objects.filter(user=user_logged_in)
     if len(trials_for_this_user) != 0:
         latest_trial = trials_for_this_user.last()
