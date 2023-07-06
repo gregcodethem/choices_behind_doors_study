9b85b65f143776251c0c8493ac23ac649fc9981e
Greg Shepley <placeholder@example.com>
2023-07-06 10:08:51 +0300

    new unittest code: test_prelim_five changed to work - failing

--- doorgame/tests/views/test_prelim_five.py
+++ doorgame/tests/views/test_prelim_five.py
@@ -2,53 +2,53 @@ from unittest.mock import patch
 
 from django.urls import resolve
 
-from doorgame.views import prelim_four
+from doorgame.views import prelim_five
 from doorgame.models import MemoryGame
 
 from .base import BaseTest
 
 
-class PrelimThreeTest(BaseTest):
-    def test_prelim_four_url_resolves_to_prelim_four_view(self):
+class PrelimFiveTest(BaseTest):
+    def test_prelim_five_url_resolves_to_prelim_five_view(self):
         self.login_temp()
-        found = resolve('/prelim_four')
-        self.assertEqual(found.func, prelim_four)
+        found = resolve('/prelim_five')
+        self.assertEqual(found.func, prelim_five)
 
 
-    def test_prelim_four_user_sees_prelim_four_page(self):
+    def test_prelim_five_user_sees_prelim_five_page(self):
         self.login_temp()
 
-        response = self.client.get('/prelim_four', follow=True)
+        response = self.client.get('/prelim_five', follow=True)
 
         html = response.content.decode('utf8')
         self.assertIn('Now play the memory task', html)
 
-    def test_prelim_four_returns_prelim_four_template(self):
+    def test_prelim_five_returns_prelim_five_template(self):
         self.login_temp()
 
-        response = self.client.get('/prelim_four', follow=True)
-        self.assertTemplateUsed(response, 'prelim_four.html')
+        response = self.client.get('/prelim_five', follow=True)
+        self.assertTemplateUsed(response, 'prelim_five.html')
 
-    def test_prelim_four_returns_continue_message(self):
+    def test_prelim_five_returns_continue_message(self):
         self.login_temp()
 
         response = self.client.get(
-            '/prelim_four',
+            '/prelim_five',
             follow=True,
         )
         html = response.content.decode('utf8')
 
         # test desired continue message is displayed:
-        self.assertIn('Continue to next page',html)
+        self.assertIn("Let's play the game",html)
 
 
-    def test_prelim_four_contains_links_to_correct_page(self):
+    def test_prelim_five_contains_links_to_correct_page(self):
         self.login_temp()
 
         response = self.client.get(
-            '/prelim_four',
+            '/prelim_five',
             follow=True,
         )
         html = response.content.decode('utf8')
 
-        self.assertIn('href= /prelim_five', html)
+        self.assertIn('href= /memory_game_initial_turn', html)
