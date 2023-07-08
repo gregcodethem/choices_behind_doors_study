7258eb2da5b58519944da3e90de6a33637ad4621
Greg Shepley <placeholder@example.com>
2023-07-08 19:52:39 +0300

    refactor, unittests: views, test_all_others.py split into test_trial_number and test_all_others, test all others passing, test trial number failing

--- doorgame/tests/views/test_all_others.py
+++ doorgame/tests/views/test_all_others.py
@@ -1,66 +1,14 @@
-from unittest import skip
-
-from django.urls import resolve
-from django.test import Client
-from django.http import HttpRequest
-
-from doorgame.views import (
-    home_page,
-    home_page_user,
-    home_page_user_unique,
-    consent_questions,
-    prelim_one,
-    prelim_one_part_b,
-    prelim_two,
-    prelim_three,
-)
-
-from doorgame.models import Choice, Trial, Result, MemoryGame, Profile
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
+
+from doorgame.models import (
+    Choice,
+    Trial,
+    Result,
+    MemoryGame,
+)
 
 from .base import BaseTest
 from config.settings import TRIAL_LIMIT
-
-
-class TrialNumberTest(BaseTest):
-
-    def test_home_page_gives_trial_number(self):
-        self.login_temp()
-        response = self.client.get('/user', follow=True)
-
-        html = response.content.decode('utf8')
-        self.assertIn('Trial number 1', html)
-
-    def test_home_page_gives_trial_number_two_for_second_trial(self):
-        user_test = User.objects.create_user(
-            'george',
-            '',
-            'somethingpassword')
-        user_test.save()
-        first_trial = Trial()
-        first_trial.user = user_test
-        first_trial.number_of_trial = 1
-        first_trial.save()
-
-        #User = get_user_model()
-        self.client.login(username='george', password='somethingpassword')
-        response = self.client.get('/user', follow=True)
-
-        html = response.content.decode('utf8')
-        self.assertIn('Trial number 2', html)
-
-    def test_trial_number_created_when_memory_game_accessed(self):
-        self.login_temp()
-        user = User.objects.get(username='temporary')
-        response = self.client.get(
-            '/user/temporary/',
-            )
-        trials_list = Trial.objects.all()
-        trial_created = Trial.objects.last()
-        number_of_trial = trial_created.number_of_trial
-
-        self.assertEqual(number_of_trial, 1)
 
 
 class MemoryGameTest(BaseTest):
---
+++ doorgame/tests/views/test_trial_number.py
@@ -0,0 +1,44 @@
+from doorgame.models import Trial
+from django.contrib.auth.models import User
+
+from .base import BaseTest
+
+
+class TrialNumberTest(BaseTest):
+
+    def test_home_page_gives_trial_number(self):
+        self.login_temp()
+        response = self.client.get('/user', follow=True)
+
+        html = response.content.decode('utf8')
+        self.assertIn('Trial number 1', html)
+
+    def test_home_page_gives_trial_number_two_for_second_trial(self):
+        user_test = User.objects.create_user(
+            'george',
+            '',
+            'somethingpassword')
+        user_test.save()
+        first_trial = Trial()
+        first_trial.user = user_test
+        first_trial.number_of_trial = 1
+        first_trial.save()
+
+        self.client.login(username='george', password='somethingpassword')
+        response = self.client.get('/user', follow=True)
+
+        html = response.content.decode('utf8')
+        self.assertIn('Trial number 2', html)
+
+    def test_trial_number_created_when_memory_game_accessed(self):
+        self.login_temp()
+        user = User.objects.get(username='temporary')
+        response = self.client.get(
+            '/user/temporary/',
+            )
+        trials_list = Trial.objects.all()
+        trial_created = Trial.objects.last()
+        number_of_trial = trial_created.number_of_trial
+
+        self.assertEqual(number_of_trial, 1)
+
