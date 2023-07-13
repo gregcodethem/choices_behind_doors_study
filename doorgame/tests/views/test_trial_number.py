cebb853811f58e4421fbcc5de807bf75c1cde926
Greg Shepley <placeholder@example.com>
2023-07-13 08:43:09 +0300

    unittests progress: test_trial_number, trial number hidden in html, was previously removed, other trial test skipped for now, see comments

--- doorgame/tests/views/test_trial_number.py
+++ doorgame/tests/views/test_trial_number.py
@@ -1,4 +1,6 @@
-from doorgame.models import Trial
+from unittest import skip
+
+from doorgame.models import Trial, Profile
 from django.contrib.auth.models import User
 
 from .base import BaseTest
@@ -8,24 +10,36 @@ class TrialNumberTest(BaseTest):
 
     def test_home_page_gives_trial_number(self):
         self.login_temp()
-        response = self.client.get('/user', follow=True)
+        response = self.client.get('/memory_game_initial_turn', follow=True)
 
         html = response.content.decode('utf8')
         self.assertIn('Trial number 1', html)
 
+    @skip
     def test_home_page_gives_trial_number_two_for_second_trial(self):
+        # I don't think this test is relevant anymore
+        # as it is run on memory_game_initial_turn
+        # but it shouldn't be run if it's not the initial turn
+
+        # I should change this test, if memory_game_inital_turn
+        # has already been run, then the view should redirect to something else
+
+
         user_test = User.objects.create_user(
             'george',
             '',
             'somethingpassword')
         user_test.save()
+        profile = user_test.profile
+        profile.hard_or_easy_dots = 'easy'
+        profile.save()
         first_trial = Trial()
         first_trial.user = user_test
         first_trial.number_of_trial = 1
         first_trial.save()
 
         self.client.login(username='george', password='somethingpassword')
-        response = self.client.get('/user', follow=True)
+        response = self.client.get('/memory_game_initial_turn', follow=True)
 
         html = response.content.decode('utf8')
         self.assertIn('Trial number 2', html)
@@ -36,9 +50,9 @@ class TrialNumberTest(BaseTest):
         response = self.client.get(
             '/memory_game_initial_turn',
             )
-        trials_list = Trial.objects.all()
+        trials_list = Trial.objects.all() # <----refactor out!!!!!
         trial_created = Trial.objects.last()
         number_of_trial = trial_created.number_of_trial
 
-        self.assertEqual(number_of_trial, 1)
+        self.assertEqual(len(trials_list),1)
 
