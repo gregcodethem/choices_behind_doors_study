2c87f6d00554fd032b234f29c0bcf540b09984b7
Greg Shepley <placeholder@example.com>
2023-07-15 13:01:54 +0300

    new code: unittest that final_pattern url redirects correctly, had to make changes to view so this works for 3 by 3

--- doorgame/tests/views/test_memory_game.py
+++ doorgame/tests/views/test_memory_game.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
 
-from doorgame.models import MemoryGame
+from doorgame.models import (
+    MemoryGame,
+    MemoryGameList,
+    Trial,
+    Result,
+    Choice
+)
+from doorgame.utils import add_memory_games
 
 from .base import BaseTest
+from .test_door_result_one import DoorResultPageTest
 from config.settings import TRIAL_LIMIT
 
 
@@ -40,3 +48,32 @@ class MemoryGameInitialTurnTest(BaseTest):
 
         self.assertTemplateUsed(response, 'home.html')
 
+class MemoryGameFinalPatternTest(DoorResultPageTest):
+
+
+    def test_final_pattern_url_redirects_to_final_door_result(self):
+        self.login_temp()
+        user = User.objects.get(username='temporary')
+
+        # An associated trial also needs to be created
+        # as this is usually created by the view:
+        # memory_game_initial_turn
+        trial = Trial()
+        trial.user = user
+        trial.number_of_trial = 1
+        trial.save()
+
+        response = self.client.post(
+            '/final_pattern',
+            {
+                'box_1': True,
+                'box_2': True,
+                'box_3': True
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/outcome_of_doorgame')
+
+    def test_final_pattern_url_saves_memory_game_box_choices(self):
+        self.fail("finish the test!")
