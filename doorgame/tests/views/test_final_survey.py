c45084c12a6b92863c32ecd37b43316b95914e83
Greg Shepley <placeholder@example.com>
2023-07-16 12:05:18 +0300

    unittest for likn to final survey -passes

---
+++ doorgame/tests/views/test_final_survey.py
@@ -0,0 +1,10 @@
+from .base import BaseTest
+
+class FinalSurveyOneTest(BaseTest):
+
+    def test_final_survey_one_link_to_correct_template(self):
+        self.login_temp()
+
+        response = self.client.get('/final_survey_one')
+
+        self.assertTemplateUsed(response, 'final_survey_one.html')
