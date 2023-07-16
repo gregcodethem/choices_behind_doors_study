549c570f181b9b0886b4847d3aede4dd0bb82f4d
Greg Shepley <placeholder@example.com>
2023-07-16 13:10:47 +0300

    ut: test final survey page 3 is saving the models - passing

--- doorgame/tests/views/test_final_survey.py
+++ doorgame/tests/views/test_final_survey.py
@@ -43,3 +43,33 @@ class FinalSurveyOneTest(BaseTest):
 
         self.assertTemplateUsed(response, 'final_survey_three.html')
 
+    def test_final_survey_three_saves_models(self):
+
+        self.login_temp()
+        user = User.objects.get(username='temporary')
+        SurveyAnswers.objects.create(user=user)
+
+        response = self.client.post(
+            '/final_survey_three_completed',
+            {'familiar': 'yes',
+             'english': 'no',
+             'age': '33',
+             'gender': 'male',
+             'education_level': 'master'}
+        )
+
+        user = User.objects.get(username='temporary')
+        survey_answers_all = SurveyAnswers.objects.filter(user=user)
+        survey_answers = survey_answers_all.last()
+        saved_familar_with_game = survey_answers.familiar
+        saved_english_native_language = survey_answers.english
+        saved_age = survey_answers.age
+        saved_gender = survey_answers.gender
+        saved_education_level = survey_answers.education_level
+
+        self.assertEqual(len(survey_answers_all), 1)
+        self.assertEqual(saved_familar_with_game,'yes')
+        self.assertEqual(saved_english_native_language,'no')
+        self.assertEqual(saved_age, 33)
+        self.assertEqual(saved_gender, 'male')
+        self.assertEqual(saved_education_level, 'master')
