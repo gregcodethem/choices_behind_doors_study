b195d08e2549d0de1c6111358ba760d7fe45a3ae
Greg Shepley <placeholder@example.com>
2023-07-24 15:45:25 +0100

    FT doorgame_and_memory_game passing

--- functional_tests/test_doorgame_and_memory_game.py
+++ functional_tests/test_doorgame_and_memory_game.py
@@ -21,7 +21,7 @@ class LayoutTest(BaseTest):
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, 'h2'))
         )
-        login_title = self.browser.find_element_by_tag_name(
+        login_title = self.browser.find_element(By.TAG_NAME,
             'h2').text
         self.assertIn('Login', login_title)
 
@@ -38,20 +38,20 @@ class LayoutTest(BaseTest):
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'col-sm-1'))
         )
-        small_box_list = self.browser.find_elements_by_class_name(
+        small_box_list = self.browser.find_elements(By.CLASS_NAME,
             "col-sm-1"
         )
         number_of_small_boxes = len(small_box_list)
         self.assertEqual(number_of_small_boxes, 9)
 
         # In the 3 by 3 grid there should be 3 dots
-        dot_pics_with_dots = self.browser.find_elements_by_xpath('//img[@src="/static/doorgame/box_with_dot.png"]')
+        dot_pics_with_dots = self.browser.find_elements(By.XPATH,'//img[@src="/static/doorgame/box_with_dot.png"]')
         number_of_dots = len(dot_pics_with_dots)
         self.assertEqual(number_of_dots, 3)
 
         # James cannot see a message displayed saying: "Can you remember the pattern from before?"
         try:
-            remember_pattern_message = self.browser.find_element_by_id(
+            remember_pattern_message = self.browser.find_element(By.ID,
                 "Welcome to the door game"
             )
             remember_pattern_message_present = True
@@ -64,8 +64,8 @@ class LayoutTest(BaseTest):
         )
 
         # user sees a box they can remember
-        box_1 = self.browser.find_element_by_id('box_1')
-        box_2 = self.browser.find_element_by_id('box_2')
+        box_1 = self.browser.find_element(By.ID,'box_1')
+        box_2 = self.browser.find_element(By.ID,'box_2')
 
         # James is automatically redirected to the door game
         time.sleep(2)
@@ -74,14 +74,14 @@ class LayoutTest(BaseTest):
             EC.presence_of_element_located((By.TAG_NAME, 'h2'))
         )
         # see text welcome page
-        game_title = self.browser.find_element_by_tag_name('h2').text
+        game_title = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('Welcome to the door game', game_title)
 
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.ID, 'door1'))
         )
         # user can see a door
-        door1 = self.browser.find_element_by_id('door1')
+        door1 = self.browser.find_element(By.ID,'door1')
         self.assertAlmostEqual(
             door1.location['x'] + door1.size['width'] / 2,
             300,
@@ -90,13 +90,13 @@ class LayoutTest(BaseTest):
 
 
         '''
-        door2 = self.browser.find_element_by_id('door2')
+        door2 = self.browser.find_element(By.ID,'door2')
         self.assertAlmostEqual(
             door2.location['x'] + door2.size['width'] / 2,
             300,
             delta=20
         )
-        door3 = self.browser.find_element_by_id('door3')
+        door3 = self.browser.find_element(By.ID,'door3')
         self.assertAlmostEqual(
             door3.location['x'] + door3.size['width'] / 2,
             480,
@@ -113,11 +113,11 @@ class DifferentChoiceTest(BaseTest):
         self.user_goes_straight_to_first_door_game_via_memory_game()
 
         # see text welcome page
-        game_title = self.browser.find_element_by_tag_name('h2').text
+        game_title = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('Welcome to the door game', game_title)
 
         # James can see a door in position 1
-        door_one = self.browser.find_element_by_id('door1')
+        door_one = self.browser.find_element(By.ID,'door1')
         image_door_one_before_click = door_one.get_attribute('src')
 
         # Check that this door is the normal door and is not red
@@ -132,7 +132,7 @@ class DifferentChoiceTest(BaseTest):
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.ID, door_number))
         )
-        door_to_choose = self.browser.find_element_by_id(door_number)
+        door_to_choose = self.browser.find_element(By.ID,door_number)
         door_to_choose.click()
 
         # wait for the image to change
@@ -141,7 +141,7 @@ class DifferentChoiceTest(BaseTest):
         )
 
         # Now the door has changed
-        door_one = self.browser.find_element_by_id('door1')
+        door_one = self.browser.find_element(By.ID,'door1')
         image_door_one_after_click = door_one.get_attribute('src')
         self.assertNotEqual(
             image_door_one_before_click,
@@ -159,13 +159,13 @@ class DifferentChoiceTest(BaseTest):
         )
 
         # User sees message that they've chosen a door
-        first_door_chosen_message = self.browser.find_element_by_tag_name(
+        first_door_chosen_message = self.browser.find_element(By.TAG_NAME,
             'h2').text
         self.assertIn('The result of your door choice', first_door_chosen_message)
 
         # James can still see which door they've chosen
         # Now the door has changed
-        door_one_result_page = self.browser.find_element_by_id('door1')
+        door_one_result_page = self.browser.find_element(By.ID,'door1')
         image_door_one_result_page = door_one_result_page.get_attribute('src')
         self.assertEqual(
             image_door_one_result_page,
@@ -175,7 +175,7 @@ class DifferentChoiceTest(BaseTest):
         # their door choice is saved
         # user logs out
         self.logout()
-        login_title = self.browser.find_element_by_tag_name(
+        login_title = self.browser.find_element(By.TAG_NAME,
             'h2').text
         self.assertIn('Login', login_title)
 
@@ -185,20 +185,20 @@ class DifferentChoiceTest(BaseTest):
             user_identifier="Ozen"
         )
         # sees text welcome page
-        game_title = self.browser.find_element_by_tag_name('h2').text
+        game_title = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('Welcome to the door game', game_title)
 
         # Ozen can chose a door
         door_number = 'door2'
         self.user_chooses_a_door(door_number)
 
-        first_door_chosen_message = self.browser.find_element_by_tag_name(
+        first_door_chosen_message = self.browser.find_element(By.TAG_NAME,
             'h2').text
         self.assertIn('The result of your door choice', first_door_chosen_message)
 
         # Ozen can still see which door they've chosen
         # Now the door has changed
-        door_two_result_page = self.browser.find_element_by_id('door2')
+        door_two_result_page = self.browser.find_element(By.ID,'door2')
         image_door_two_result_page = door_two_result_page.get_attribute('src')
         self.assertEqual(
             image_door_two_result_page,
@@ -213,19 +213,19 @@ class DifferentChoiceTest(BaseTest):
             user_identifier="Bob"
         )
         # see text welcome page
-        game_title = self.browser.find_element_by_tag_name('h2').text
+        game_title = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('Welcome to the door game', game_title)
 
         # Bob can chose a door
         door_number = 'door3'
         self.user_chooses_a_door(door_number)
 
-        first_door_chosen_message = self.browser.find_element_by_tag_name(
+        first_door_chosen_message = self.browser.find_element(By.TAG_NAME,
             'h2').text
         self.assertIn('The result of your door choice', first_door_chosen_message)
 
         # Now the door has changed
-        door_three_result_page = self.browser.find_element_by_id('door3')
+        door_three_result_page = self.browser.find_element(By.ID,'door3')
         image_door_three_result_page = door_three_result_page.get_attribute('src')
         self.assertEqual(
             image_door_three_result_page,
@@ -283,15 +283,15 @@ class SecondChoiceTest(BaseTest):
             EC.presence_of_element_located((By.ID, 'monty_speech_bubble'))
         )
         # John sees message that they can change door
-        new_choice_message = self.browser.find_element_by_id(
+        new_choice_message = self.browser.find_element(By.ID,
             'monty_speech_bubble').text
         self.assertIn("I am revealing one of the two goats.", new_choice_message)
 
         # John can see a goat in either door two or door three
-        goat_two = self.browser.find_elements_by_id(
+        goat_two = self.browser.find_elements(By.ID,
             'open_door_goat2'
         )
-        goat_three = self.browser.find_elements_by_id(
+        goat_three = self.browser.find_elements(By.ID,
             'open_door_goat3'
         )
         self.assertTrue(
@@ -299,7 +299,7 @@ class SecondChoiceTest(BaseTest):
             "Neither 'open_door_goat3' nor 'open_door_goat2' was found"
         )
         # check that there is no goat image for door 1:
-        goat_one = self.browser.find_elements_by_id(
+        goat_one = self.browser.find_elements(By.ID,
             'open_door_goat1'
         )
         self.assertTrue(
@@ -309,11 +309,11 @@ class SecondChoiceTest(BaseTest):
 
 
         # John can see option to keep or change their choice
-        keep_door_link = self.browser.find_element_by_id('keep_door_link')
+        keep_door_link = self.browser.find_element(By.ID,'keep_door_link')
         keep_door_text = keep_door_link.text
         self.assertEqual(keep_door_text,'Stick with door 1',msg="keep door text not found or incorrect")
 
-        change_door_link = self.browser.find_element_by_id('change_door_link')
+        change_door_link = self.browser.find_element(By.ID,'change_door_link')
         change_door_text = change_door_link.text
         self.assertTrue(
             change_door_text=="Switch to door 2" or change_door_text=="Switch to door 3",
@@ -326,21 +326,21 @@ class SecondChoiceTest(BaseTest):
         # When John has done the process the required number
         # of times, he then sees a regret page
         # John sees a regret page:
-        regret_message = self.browser.find_element_by_tag_name('h2').text
+        regret_message = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('To what extent would you experience regret', regret_message)
 
         # They click on the number 1
-        regret_one = self.browser.find_element_by_id('regret_one')
+        regret_one = self.browser.find_element(By.ID,'regret_one')
         regret_one.click()
 
         # Then they click submit
-        submit_button = self.browser.find_element_by_id('complete-the-survey')
+        submit_button = self.browser.find_element(By.ID,'complete-the-survey')
         submit_button.click()
 
 
         # John sees the grid from before
 
-        final_pattern_message = self.browser.find_element_by_id(
+        final_pattern_message = self.browser.find_element(By.ID,
             'final_pattern_message').text
         self.assertIn(
             'Can you remember the pattern from before?', final_pattern_message
@@ -349,9 +349,9 @@ class SecondChoiceTest(BaseTest):
             EC.presence_of_element_located((By.ID, 'div_box_1'))
         )
         # John sees blank boxes that they can click
-        box_1 = self.browser.find_element_by_id('div_box_1')
-        box_2 = self.browser.find_element_by_id('div_box_2')
-        box_3 = self.browser.find_element_by_id('div_box_3')
+        box_1 = self.browser.find_element(By.ID,'div_box_1')
+        box_2 = self.browser.find_element(By.ID,'div_box_2')
+        box_3 = self.browser.find_element(By.ID,'div_box_3')
 
         box_1.click()
         box_2.click()
@@ -361,7 +361,7 @@ class SecondChoiceTest(BaseTest):
         # I need another FT, where they play twice, then
         # a code block equivalent to this will go there
         # user goes back to first screen
-        play_again_link = self.browser.find_element_by_id(
+        play_again_link = self.browser.find_element(By.ID,
             'play_again_link')
         play_again_link.click()
 
@@ -373,7 +373,7 @@ class SecondChoiceTest(BaseTest):
             EC.presence_of_element_located((By.ID, 'final_choice_message'))
         )
 
-        final_door_message = self.browser.find_element_by_tag_name('h2').text
+        final_door_message = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('The result of your final door choice', final_door_message)
 
 
@@ -400,7 +400,7 @@ class SecondChoiceTest(BaseTest):
         self.user_chooses_a_door("door1")
 
         # John sees option that they can change door
-        change_door_link = self.browser.find_element_by_id('change_door_link')
+        change_door_link = self.browser.find_element(By.ID,'change_door_link')
         change_door_text = change_door_link.text
         self.assertTrue(
             change_door_text == "Switch to door 2" or change_door_text == "Switch to door 3",
@@ -413,15 +413,15 @@ class SecondChoiceTest(BaseTest):
         # When John has done the process the required number
         # of times, he then sees a regret page
         # John sees a regret page:
-        regret_message = self.browser.find_element_by_tag_name('h2').text
+        regret_message = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('To what extent would you experience regret', regret_message)
 
         # They click on the number 1
-        regret_one = self.browser.find_element_by_id('regret_one')
+        regret_one = self.browser.find_element(By.ID,'regret_one')
         regret_one.click()
 
         # Then they click submit
-        submit_button = self.browser.find_element_by_id('complete-the-survey')
+        submit_button = self.browser.find_element(By.ID,'complete-the-survey')
         submit_button.click()
 
         WebDriverWait(self.browser, 10).until(
@@ -429,7 +429,7 @@ class SecondChoiceTest(BaseTest):
         )
 
         # John sees the grid from before
-        final_pattern_message = self.browser.find_element_by_id(
+        final_pattern_message = self.browser.find_element(By.ID,
             'final_pattern_message').text
         self.assertIn(
             'Can you remember the pattern from before?', final_pattern_message
@@ -438,9 +438,9 @@ class SecondChoiceTest(BaseTest):
             EC.presence_of_element_located((By.ID, 'div_box_1'))
         )
         # John sees blank boxes that they can click
-        box_1 = self.browser.find_element_by_id('div_box_1')
-        box_2 = self.browser.find_element_by_id('div_box_2')
-        box_3 = self.browser.find_element_by_id('div_box_3')
+        box_1 = self.browser.find_element(By.ID,'div_box_1')
+        box_2 = self.browser.find_element(By.ID,'div_box_2')
+        box_3 = self.browser.find_element(By.ID,'div_box_3')
 
         box_1.click()
         box_2.click()
@@ -450,7 +450,7 @@ class SecondChoiceTest(BaseTest):
         # I need another FT, where they play twice, then
         # a code block equivalent to this will go there
         # user goes back to first screen
-        play_again_link = self.browser.find_element_by_id(
+        play_again_link = self.browser.find_element(By.ID,
             'play_again_link')
         play_again_link.click()
 
@@ -462,7 +462,7 @@ class SecondChoiceTest(BaseTest):
             EC.presence_of_element_located((By.ID, 'final_choice_message'))
         )
 
-        final_door_message = self.browser.find_element_by_tag_name('h2').text
+        final_door_message = self.browser.find_element(By.TAG_NAME,'h2').text
         self.assertIn('The result of your final door choice', final_door_message)
 
         self.logout()
